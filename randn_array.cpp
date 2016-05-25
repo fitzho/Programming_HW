9fab47af0e2dbc62a95605a6f3d333a67bae3909
Fitz_Ho <fitz_ho@htc.com>
2016-05-25 11:34:25 +0800

    Random_Array_Version_2

--- randn_array.cpp
+++ randn_array.cpp
@@ -6,44 +6,45 @@
 #include <time.h>
 using namespace std; 
 
-int a[10]={};
-int b;
-int c;
-int correct_pos;
-int correct_val;
+#define NUM 412
 
-void arrangement(void);
+static int a[NUM];
+
+int *generate_random_list(int *);
 int num(void);
-void catch1(int,int);
-void correct(int,int,int);
+int show(int a[],int);
+int modify(int a[],int,int);
 
 
 int main(void) { 
 
-	arrangement();
-	
-	num();
-	
-	catch1(a[10],b);
 
-	correct(a[10],correct_pos,correct_val);
+	generate_random_list(a);
+	
+	show(a,num());
 
+	
+	int modify_pos,modify_val;
+	modify(a,modify_pos,modify_val);
+	
+	
     return 0;
 }
 
-void arrangement(void)
+//int generate_random_list( "a pointer to an array" )
+int *generate_random_list(int *a)
 {
-	extern int a[10];
+
 	int c;
 	
 	srand( (unsigned)time(NULL) );  //時間為每次執行的變數 
 
 	
-	for (int temp = 0 ; temp < 10 ; temp++)  // temp <= (10-1)  // a 裡面的index 
+	for (int temp = 0 ; temp < NUM ; temp++)  // temp <= (10-1)  // a 裡面的index 
 	{
 			
 		
-			c=rand()%10+1;  //產生一個隨機數 
+			c=rand()%NUM+1;  //產生一個隨機數 1 <= c <= NUM
 			a[temp]= c;  //隨機數存放給 a 
 			
 			for (int temp2=0 ; temp2<temp ; temp2++)
@@ -61,49 +62,47 @@ void arrangement(void)
 		
 	cout << "array a["  << temp << "] = " << a[temp] << endl;	
 	}
-
+	return a;
 }
 
 int num(void)  //產生一個隨機數 
 {
-	extern int b;
-	b=rand()%10 + 1;
+	int random_position;
+	random_position=rand()%NUM; // 0 <= random_position <NUM-1
 	
-	cout << "b = " << b <<endl;
-	return b;
+	cout << "random_position = " << random_position <<endl;
+	return random_position;
 }
 
-void catch1(int a,int b)  //顯示出 a 在 隨機數位址上的值 
+int show(int a[],int b)  //顯示出 a 在 隨機數位址上的值 
 {
-	extern int a[10];
-	extern int b;
 	
 	cout << "position = " << b  << endl;
 	
 	cout <<  "a[" << b <<"] = "  <<  a[b] << endl;
-
-}
-void 	correct(int a, int correct_pos, int correct_val)  //更改指定位址 與 值 
-{
-	extern int a[10];
-	extern int correct_pos;
-	extern int correct_val;
 	
-	cout << "correct position = " << endl;  //輸入更改位址 
-	cin  >> correct_pos  ;
+	return 0;
+}
+
+
+int modify(int a[], int modify_pos, int modify_val)  //更改指定位址 與 值 
+{
+	
+	cout << "modify position = " << endl;  //輸入更改位址 
+	cin  >> modify_pos  ;
 	cout << endl;
 	
-	cout << "correct value = " << endl;  //輸入要更改的值 
-	cin  >> correct_val  ;
+	cout << "modify value = " << endl;  //輸入要更改的值 
+	cin  >> modify_val  ;
 	cout << endl;	
 	
-	a[correct_pos]=correct_val;  //指定值給 a 位址 
+	a[modify_pos]=modify_val;  //指定值給 a 位址 
 	
-/*	
-	cout << "a["<< correct_pos << "] = " << a[correct_pos] << endl;
-	for (int temp = 0; temp < 10 ; temp++)
+
+	cout << "a["<< modify_pos << "] = " << a[modify_pos] << endl;
+	for (int temp = 0; temp < NUM ; temp++)
 	{
 		cout<< "a[" << temp << "] = " << a[temp] << endl;
 	}
-*/
+	return 0;
 }
