0d0009c6f38d67ca2817e9307c2c67f520927ba3
Fitz_Ho <fitz_ho@htc.com>
2016-05-26 15:30:25 +0800

    Random_Array_Version_3

--- randn_array.cpp
+++ randn_array.cpp
@@ -6,64 +6,79 @@
 #include <time.h>
 using namespace std; 
 
-#define NUM 412
+#define NUM 9
+#define SUCCESS 0
+#define FAILED  -1
+/*
+int generate_random_list(int (*a_list)[NUM]);
+int generate_random_list( "a pointer to an array" )
+int generate_random_list(int (*a_list)[NUM])
+{
+	int random_number;
+	srand( (unsigned)time(NULL) );  //時間為每次執行的變數 
+	for (int temp = 0 ; temp < NUM ; temp++)  // temp <= (10-1)  // a 裡面的index 
+	{
+		random_number=rand()%NUM+1;  //產生一個隨機數 1 <= c <= NUM
+		//a_list[temp]= random_number;  //隨機數存放給 a 
+		//*(a_list[0]+temp)=random_number;
+		*(a_list[0]+temp)=random_number;
+		
+		for (int temp2=0 ; temp2<temp ; temp2++)
+		{
+			while (*(a_list[0]+temp)==(*(a_list[0]+temp2)))  //當有重複的情況 ，temp減一回去 
+			{
+				temp--;
+				break;
+			}
+		}
+	} // End of for (int temp = 0 ; temp < NUM ; temp++)
+//	return SUCCESS;
+}
+*/
 
-static int a[NUM];
-
-int *generate_random_list(int *);
+//int *generate_random_list(int *);
+int generate_random_list(int *);
 int num(void);
 int show(int a[],int);
 int modify(int a[],int,int);
+int show_random_list(int *);
 
-
-int main(void) { 
-
-
-	generate_random_list(a);
-	
-	show(a,num());
-
-	
+int main(void)
+{ 
+	int random_list[NUM];
+	//ret = generate_random_list(random_list);
+	//generate_random_list(&random_list);
+	generate_random_list(random_list);
+	show(random_list,num());
+	show_random_list(random_list);
 	int modify_pos,modify_val;
-	modify(a,modify_pos,modify_val);
-	
+	modify(random_list,modify_pos,modify_val);
 	
     return 0;
 }
 
-//int generate_random_list( "a pointer to an array" )
-int *generate_random_list(int *a)
+
+int generate_random_list(int *a_list)
 {
-
-	int c;
+	int random_number;
+	int max_num=NUM;
+	int array_store[NUM];
 	
-	srand( (unsigned)time(NULL) );  //時間為每次執行的變數 
-
-	
-	for (int temp = 0 ; temp < NUM ; temp++)  // temp <= (10-1)  // a 裡面的index 
+	for (int temp = 0 ; temp < NUM ; temp++) //創一個存放1~NUM的陣列 
 	{
-			
-		
-			c=rand()%NUM+1;  //產生一個隨機數 1 <= c <= NUM
-			a[temp]= c;  //隨機數存放給 a 
-			
-			for (int temp2=0 ; temp2<temp ; temp2++)
-			{
-				
-				
-				while (a[temp]==a[temp2])  //當有重複的情況 ，temp減一回去 
-				{
-					temp--;
-					break;
-				}
-				
-			}
-
-		
-	cout << "array a["  << temp << "] = " << a[temp] << endl;	
+		array_store[temp]=temp+1;
 	}
-	return a;
+	srand( (unsigned)time(NULL) );  //時間為每次執行的變數 
+	for (int temp = 0 ; temp < NUM ; temp++)   // temp 是 a_list 裡面的index 
+	{
+		random_number=rand()%max_num;  //產生一個隨機數 0 <= c <= max_num
+		a_list[temp] =array_store[random_number];  //抓取在隨機數random_number位置的array_store，存放給 a_list[temp]
+		array_store[random_number]=array_store[max_num-1];//在隨機數random_number位置的array_store的值，改為 array_store 上最後一個值 
+		max_num--;
+	} 
+	return SUCCESS;
 }
+
 
 int num(void)  //產生一個隨機數 
 {
@@ -74,35 +89,43 @@ int num(void)  //產生一個隨機數
 	return random_position;
 }
 
-int show(int a[],int b)  //顯示出 a 在 隨機數位址上的值 
-{
-	
-	cout << "position = " << b  << endl;
-	
-	cout <<  "a[" << b <<"] = "  <<  a[b] << endl;
-	
+
+int show(int a_list[],int show_position)  //顯示出 a 在 隨機數位址上的值 
+{	
+	cout << "position = " << show_position  << endl;
+	cout <<  "a[" << show_position <<"] = "  <<  a_list[show_position] << endl;
 	return 0;
 }
 
 
-int modify(int a[], int modify_pos, int modify_val)  //更改指定位址 與 值 
+//show_random_list(int *a, num)
+show_random_list(int *a_list)
 {
-	
-	cout << "modify position = " << endl;  //輸入更改位址 
-	cin  >> modify_pos  ;
-	cout << endl;
-	
+	for (int list_index=0;list_index<NUM;list_index++)
+	{
+		cout << "array list["  << list_index << "] = " << a_list[list_index] << endl;
+	}
+}
+
+
+int modify(int a_list[], int modify_pos, int modify_val)  //更改指定位址 與 值 
+{
+	do{	
+		cout << "modify position = " << endl;  //輸入更改位址 
+		cin  >> modify_pos  ;
+		cout << endl;
+	}while (modify_pos>NUM);//判斷輸入位址是否超過範圍
+
 	cout << "modify value = " << endl;  //輸入要更改的值 
 	cin  >> modify_val  ;
 	cout << endl;	
-	
-	a[modify_pos]=modify_val;  //指定值給 a 位址 
-	
 
-	cout << "a["<< modify_pos << "] = " << a[modify_pos] << endl;
+	a_list[modify_pos]=modify_val;  //指定值給 a 位址 
+	cout << "a["<< modify_pos << "] = " << a_list[modify_pos] << endl;
+	cout << endl;
 	for (int temp = 0; temp < NUM ; temp++)
 	{
-		cout<< "a[" << temp << "] = " << a[temp] << endl;
+		cout<< "a[" << temp << "] = " << a_list[temp] << endl;
 	}
 	return 0;
 }
